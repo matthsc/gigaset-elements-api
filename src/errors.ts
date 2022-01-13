91da75d37824c67e4034934a1a77f0d2a48d0619
Matthias Schwende <matthsc@gmx.net>
2022-01-13 20:43:51 +0100

    fix prototype chain of custom errors for ES5(CommonJS build

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,9 @@
 /** custom error object for network errors */
 export class NetworkError extends Error {
   constructor(public readonly parent: Error) {
-    super(parent.message);
+    super(parent.message); // 'Error' breaks prototype chain here for ES5/CommonJS builds
+    this.name = "NetworkError";
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
   }
 }
 
@@ -13,6 +15,8 @@ export class EndpointError extends Error {
     public readonly uri: string,
     message: string,
   ) {
-    super(message);
+    super(message); // 'Error' breaks prototype chain here for ES5/CommonJS builds
+    this.name = "EndpointError";
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
   }
 }
