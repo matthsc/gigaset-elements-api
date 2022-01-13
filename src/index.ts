e98dfe75546782fedbf6c7e1926f03e2b0c2bb54
Matthias Schwende <matthsc@gmx.net>
2022-01-13 20:59:03 +0100

    include test-data in npm package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from "./api";
 export * from "./errors";
 export * from "./model";
+
+export {
+  loadBaseStations,
+  loadElements,
+  loadEvents,
+  retrieveAndPrepareTestData,
+} from "../test-data";
