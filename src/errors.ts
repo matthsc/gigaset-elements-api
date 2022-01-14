ed0dd12a5f2a14ddc0976524c3da843d1795c473
Matthias Schwende <matthsc@gmx.net>
2022-01-14 12:32:46 +0100

    target modern es in commonjs build

--- src/errors.ts
+++ src/errors.ts
@@ -1,9 +1,7 @@
 /** custom error object for network errors */
 export class NetworkError extends Error {
   constructor(public readonly parent: Error) {
-    super(parent.message); // 'Error' breaks prototype chain here for ES5/CommonJS builds
-    this.name = "NetworkError";
-    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    super(parent.message);
   }
 }
 
@@ -15,8 +13,6 @@ export class EndpointError extends Error {
     public readonly uri: string,
     message: string,
   ) {
-    super(message); // 'Error' breaks prototype chain here for ES5/CommonJS builds
-    this.name = "EndpointError";
-    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    super(message);
   }
 }
